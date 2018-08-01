b9b4312d236de4fcaa27f028efddfee3e61ea7ae
Stéphane Lavergne <lis@imars.com>
2018-08-01 08:39:36 -0400

    Add monthly target preceding daily

--- harvest-dailytarget.js
+++ harvest-dailytarget.js
@@ -1,8 +1,9 @@
 /**
  * Harvest Daily Target Hours (bookmarklet)
  *
- * Adds a number input (default: 8.00) for target daily hours.
- * Shows hours left and dynamic ETA near total.
+ * Adds number inputs for monthly/daily target hours.
+ * Shows hours left this month.
+ * Shows hours left today and ETA near total.
  *
  * @package   harvest-bookmarklet-dailytarget
  * @author    Stéphane Lavergne <https://github.com/vphantom/>
@@ -12,21 +13,40 @@
 (function() {
 
 	// Create input for target hours
-	$('.timesheet-header .fl-right').prepend('Target: <input type="number" id="__daily_target" value="8.00" step="0.01" min="0.00" max="24.00" placeholder="8.00">');
-	var input = $('#__daily_target');
+	$('.timesheet-header .fl-right').prepend(
+		'<B>Targets:</B> '
+		+ ' Month: <input type="number" id="__m_tgt" value="0" min="0" max="744" step="1">'
+		+ ' Daily: <input type="number" id="__d_tgt" value="8.00" min="0" max="24" step="0.01">'
+	);
+	var
+		monthly = $('#__m_tgt'),
+		daily = $('#__d_tgt')
+	;
 
-	// Update ETA
+	// Update ETA on daily input change and periodically
 	var update_eta = function() {
 		var
-			target = Number(input.val()),
-			div    = $('.day-view-total'),
-			done   = Number(div.find('span').text()),
-			delta  = Math.max(0, target - done),
-			eta    = new Date((new Date()).getTime() + (delta * 3600000)),
-			etaTxt = eta.getHours() + ':' + (eta.getMinutes() < 10 ? '0' : '') + eta.getMinutes()
+			dailyVal = Number(daily.val()),
+			div      = $('.day-view-total'),
+			done     = Number(div.find('span').text()),
+			delta    = Math.max(0, dailyVal - done),
+			eta      = new Date((new Date()).getTime() + (delta * 3600000)),
+			etaTxt   = eta.getHours() + ':' + (eta.getMinutes() < 10 ? '0' : '') + eta.getMinutes()
 		;
-		div.html('(' + delta.toFixed(2) + ' to ' + etaTxt + ') &nbsp; Total: <span>' + done.toFixed(2) + '</span>');
+		div.html('(' + delta.toFixed(2) + 'h until ' + etaTxt + ') &nbsp; Total: <span>' + done.toFixed(2) + '</span>');
 	};
 	setInterval(update_eta, 2000);
-	input.on('input change', update_eta);
+	daily.on('input change', update_eta);
+
+	// Update daily input on monthly input change
+	var update_daily = function () {
+		var
+			monthlyVal    = Number(monthly.val()),
+			now           = new Date(),
+			monthDaysLeft = (new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate()) - now.getDate()
+		;
+		daily.val(Number(monthlyVal / monthDaysLeft).toFixed(2));
+		update_eta();
+	};
+	monthly.on('input change', update_daily);
 })();
