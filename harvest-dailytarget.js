cb972f7071460f669483096303d90087c1a7dc66
Stéphane Lavergne <lis@imars.com>
2020-07-08 07:39:17 -0400

    Consolidate UI in single DIV away from dynamic content

--- harvest-dailytarget.js
+++ harvest-dailytarget.js
@@ -7,34 +7,36 @@
  *
  * @package   harvest-bookmarklet-dailytarget
  * @author    Stéphane Lavergne <https://github.com/vphantom/>
- * @copyright 2018 Stéphane Lavergne
+ * @copyright 2018-2020 Stéphane Lavergne
  * @license   https://opensource.org/licenses/MIT  MIT
  */
 (function() {
 
 	// Create input for target hours
-	$('.js-timesheet-header > div').first().prepend(
-		' Week: <input type="number" id="__w_tgt" value="40" min="0" max="168" step="1">'
+	$('.js-timesheet-view').first().prepend(
+		'<div><strong>Target Hours</strong> -'
+		+ ' Week: <input type="number" id="__w_tgt" value="40" min="0" max="168" step="1">'
 		+ ' Daily: <input type="number" id="__d_tgt" min="0" max="24" step="0.01">'
 		+ ' Days/week: <input type="number" id="__w_l" value="5" min="1" max="7" step="1">'
+		+ ' ETA: <span id="__eta"></span></div>'
 	);
 	var
 		weekly = $('#__w_tgt'),
 		weekDays = $('#__w_l'),
-		daily = $('#__d_tgt')
+		daily = $('#__d_tgt'),
+		etas = $('#__eta')
 	;
 
 	// Update ETA on daily input change and periodically
 	var update_eta = function() {
 		var
 			dailyVal = Number(daily.val()),
-			div      = $('.day-view-total'),
-			done     = Number(div.find('span').text()),
+			done     = Number($('.day-view-total span').text()),
 			delta    = Math.max(0, dailyVal - done),
 			eta      = new Date((new Date()).getTime() + (delta * 3600000)),
 			etaTxt   = eta.getHours() + ':' + (eta.getMinutes() < 10 ? '0' : '') + eta.getMinutes()
 		;
-		div.html('(' + delta.toFixed(2) + 'h until ' + etaTxt + ') &nbsp; Total: <span>' + done.toFixed(2) + '</span>');
+		etas.html(delta.toFixed(2) + 'h until ' + etaTxt);
 	};
 	setInterval(update_eta, 2000);
 	daily.on('input change', update_eta);
