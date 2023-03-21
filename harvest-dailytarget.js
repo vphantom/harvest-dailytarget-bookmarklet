86cdce8163b81d143775d047111048cdb3547f38
Stéphane Lavergne <lis@imars.com>
2023-03-20 21:44:09 -0400

    New concept of minimum work
    The default hard-coded ratio is 3/5 (60%).  This displays a secondary
    ETA for reaching that intermediary goal.

--- harvest-dailytarget.js
+++ harvest-dailytarget.js
@@ -7,43 +7,58 @@
  *
  * @package   harvest-bookmarklet-dailytarget
  * @author    Stéphane Lavergne <https://github.com/vphantom/>
- * @copyright 2018-2021 Stéphane Lavergne
+ * @copyright 2018-2023 Stéphane Lavergne
  * @license   https://opensource.org/licenses/MIT  MIT
  */
+/* eslint-env browser, jquery */
 (function() {
-	var hours_per_week = 40,
-		days_per_week = 5;
+	var hours_per_week = 37.5,
+		days_per_week = 5,
+		min_ratio = 3 / 5;
 
 	// Create input for target hours
 	$('.js-timesheet-view')
 		.first()
 		.prepend(
-			'<div style="background:#f8f8f8;border:1px solid #e0e0e0;padding:0.5em;width:fit-content;margin:0 auto 1em auto;"><b style="font-size:150%;margin-right:1em">Targets</b>' +
-				' Week: <input type="number" id="__w_tgt" value="' +
+			'<div style="background:#f8f8f8;border:1px solid #e0e0e0;padding:0.5em;width:fit-content;margin:0 auto 1em auto;text-align:right"><b style="font-size:150%;margin-right:1em">Targets</b>' +
+				' W:<input title="Weekly hours" type="number" id="__w_tgt" value="' +
 				hours_per_week +
-				'" min="0" max="168" step="1">' +
-				' Daily: <input type="number" id="__d_tgt" min="0" max="24" step="0.01">' +
-				' Days/week: <input type="number" id="__w_l" value="' +
+				'" min="0" max="99" step="1">' +
+				' D/W:<input title="Days per week" type="number" id="__w_l" value="' +
 				days_per_week +
 				'" min="1" max="7" step="1">' +
-				' <b style="font-size:150%;margin-left:1em">ETA: <span id="__eta"></span></b></div>'
+				' D:<input title="Daily hours" type="number" id="__d_tgt" min="0" max="24" step="0.01">' +
+				' <b style="font-size:150%;margin-left:1em">ETA: <span id="__eta"></span></b>' +
+				'<br>(<i>min: <span id="__meta"></span></i>)</div>'
 		);
 	var weekly = $('#__w_tgt'),
 		weekDays = $('#__w_l'),
 		daily = $('#__d_tgt'),
-		etas = $('#__eta');
+		etas = $('#__eta'),
+		metas = $('#__meta');
 	// Update ETA on daily input change and periodically
 	var update_eta = function() {
 		var dailyVal = Number(daily.val()),
 			done = Number($('.is-today .pds-text-sm').text()),
 			delta = Math.max(0, dailyVal - done),
 			eta = new Date(new Date().getTime() + delta * 3600000),
-			etaTxt =
+			meta = new Date(new Date().getTime() + delta * min_ratio * 3600000);
+		etas.html(
+			delta.toFixed(2) +
+				'h until ' +
 				eta.getHours() +
 				':' +
 				(eta.getMinutes() < 10 ? '0' : '') +
-				eta.getMinutes();
-		etas.html(delta.toFixed(2) + 'h until ' + etaTxt);
+				eta.getMinutes()
+		);
+		metas.html(
+			(delta * min_ratio).toFixed(2) +
+				'h until ' +
+				meta.getHours() +
+				':' +
+				(meta.getMinutes() < 10 ? '0' : '') +
+				meta.getMinutes()
+		);
 	};
 	setInterval(update_eta, 2000);
 	daily.on('input change', update_eta);
